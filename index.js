bc67e3e7387b6811eb35d4ce8b2df977179e44fe
Jackson Prince <jdprince555@gmail.com>
2019-03-21 00:55:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var newRankedList = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < newRankedList.length; i++) {
+    newRankedList[i].textContent = parseInt(newRankedList[i], 10) + n;
+  }
+  return newRankedList;
+}
+
+function deepestChild() {
+  var x = '#grand-node'
+  while (document.querySelector(x).children.length > 0) {
+    x += " div";
+  }
+  return document.querySelector(x);
+}
